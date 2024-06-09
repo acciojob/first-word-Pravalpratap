513254f1f5b3a2221975ebe617ffd3b0480a00a7
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-09 13:30:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+ if (str === '') {
+    return '';
+  }
+  
+  // Find the index of the first space
+  let firstSpaceIndex = str.indexOf(' ');
+  
+  // If there is no space, return the entire string
+  if (firstSpaceIndex === -1) {
+    return str;
+  }
+  
+  // Return the substring from the start up to the first space
+  return str.substring(0, firstSpaceIndex);
 }
 
 // Do not change the code below
