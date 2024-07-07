e2bdfbd282c333c7ad067ed0d9d61af7d4e112a8
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-07-07 14:07:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 function firstWord(str) {
- if (str === '') {
-    return '';
-  }
-  
-  // Find the index of the first space
-  let firstSpaceIndex = str.indexOf(' ');
-  
-  // If there is no space, return the entire string
-  if (firstSpaceIndex === -1) {
-    return str;
-  }
-  
-  // Return the substring from the start up to the first space
-  return str.substring(0, firstSpaceIndex);
+ const trimmedStr = str.trim();
+    
+    // Find the index of the first space
+    const spaceIndex = trimmedStr.indexOf(' ');
+    
+    // If no space found, return the whole trimmed string
+    if (spaceIndex === -1) {
+        return trimmedStr;
+    }
+    
+    // Return the substring from start to the first space
+    return trimmedStr.substring(0, spaceIndex);
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
